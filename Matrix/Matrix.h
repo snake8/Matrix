f8be511bd160056ef64dbc169cb9a6ecd4aeb412
snake8 <spoodiman4@gmail.com>
2017-10-01 17:32:58 +0300

    Update Matrix.h
    Add some methods

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -9,7 +9,8 @@
 #include <vector>
 #include <iostream>
 #include <string>
-#include "TypesCheck.h" 
+#include <random>
+#include "TypesCheck.h"
 #ifndef Matrix_h
 #define Matrix_h
 
@@ -24,10 +25,11 @@ public:
     Matrix(size_t, size_t);
     size_t size1();
     size_t size2();
+    void resizeMatrix(size_t rows, size_t columns);
+    void insertVector(std::vector<T>* temp);
     T& operator()(size_t, size_t);
-    void operator=(T);
+    void operator=(T value);
     void operator+=(T);
-    Matrix<T> transpose();
 };
 
 template<typename T>
@@ -47,9 +49,32 @@ size_t Matrix<T>::size2() {
 }
 
 template<typename T>
+void Matrix<T>::resizeMatrix(size_t rows, size_t columns) {
+    this->matrix.resize(rows);
+    for (size_t i = 0; i < rows; i++)
+        this->matrix[i].resize(columns);
+}
+
+
+template<typename T>
+void Matrix<T>::insertVector(std::vector<T>* temp) {
+    if (temp != nullptr)
+        this->matrix.push_back(*temp);
+}
+
+template<typename T>
 T& Matrix<T>::operator()(size_t row, size_t col) {
     return this->matrix[row][col];
 }
+
+template<typename T>
+void Matrix<T>::operator=(T value) {
+    for (size_t i = 0; i < this->matrix.size(); i++) {
+        for (size_t j = 0; j < this->matrix[i].size(); j++)
+            this->matrix[i][j] = matrix(i, j);
+    }
+}
+
 template<typename T>
 std::ostream& operator<<(std::ostream& os, Matrix<T> matrix) {
     for (size_t i = 0; i < matrix.size1(); i++) {
@@ -57,19 +82,23 @@ std::ostream& operator<<(std::ostream& os, Matrix<T> matrix) {
             os << matrix(i, j) << " ";
         std::cout << std::endl;
     }
+    std::cout << "Number of rows: " << matrix.size1() << std::endl;
+    std::cout << "Number of columns: " << matrix.size2() << std::endl;
     return os;
 }
+
+
 template<typename T>
-void Matrix<T>::operator=(T value) {
-    for (size_t i = 0; i < this->matrix.size(); i++) {
-        for (size_t j = 0; j < this->matrix[i].size(); j++)
-            this->matrix[i][j] = value;
-    }
+bool operator==(Matrix<T>& matrix1, Matrix<T>& matrix2) {
+    if (matrix1.size1() == matrix2.size1() && matrix2.size2() == matrix2.size2())
+        return true;
+    else
+        return false;
 }
 
 template<typename T>
 Matrix<T> matrixSum(Matrix<T>& matrix1, Matrix<T> matrix2) {
-    if (matrix1.size1() == matrix2.size1() && matrix1.size2() == matrix2.size2()) {
+    if (matrix1 == matrix2) {
         Matrix<T> resultMatrix(matrix1.size1(), matrix2.size2());
         for (size_t i = 0; i < resultMatrix.size1(); i++) {
             for (size_t j = 0; j < resultMatrix.size2(); j++)
@@ -79,6 +108,7 @@ Matrix<T> matrixSum(Matrix<T>& matrix1, Matrix<T> matrix2) {
     } else
         throw "Matrixes are not equal!";
 }
+
 template<typename T>
 Matrix<T> operator+(Matrix<T>& matrix1, Matrix<T>& matrix2) {
     Matrix<T> sumResult(matrix1.size1(), matrix2.size2());
@@ -96,7 +126,7 @@ Matrix<T> matrixMin(Matrix<T>& matrix1, Matrix<T>& matrix2) {
     if (IsTypeChar<T>::value || IsTypeString<T>::value) {
         throw "Matrices type is uncorrect!";
     } else {
-        if (matrix1.size1() == matrix2.size1() && matrix1.size2() == matrix2.size2()) {
+        if (matrix1 == matrix2) {
             Matrix<T> minResult(matrix1.size1(), matrix2.size2());
             for (size_t i = 0; i < minResult.size1(); i++) {
                 for (size_t j = 0; j < minResult.size2(); j++)
@@ -141,7 +171,7 @@ Matrix<T> operator*(Matrix<T>& matrix1, Matrix<T>& matrix2) {
     Matrix<T> multiplicationResult(matrix1.size1(), matrix2.size2());
     try {
         multiplicationResult = matrixMultiplication(matrix1, matrix2);
-    } catch (char const* error) {
+    } catch(char const* error) {
         std::cout << error << std::endl;
     }
     return multiplicationResult;
@@ -156,6 +186,8 @@ Matrix<T> transpose(Matrix<T>* matrix) {
     }
     return *transposedMatrix; 
 }
+
+
 
 template<typename T>
 void randomMatrixValuesGenerator(Matrix<T>& matrix, T from, T to) {
